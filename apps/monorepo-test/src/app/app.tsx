fd6a8bb7ecd92cd371e1a05fe97b140da944d6e0
Vincent Vu <vinceny.fe@gmail.com>
2022-06-08 20:41:55 -0700

    initial commit

---
+++ apps/monorepo-test/src/app/app.tsx
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from 'react';
+import { Message } from '@hacksc-platforms/api-interfaces';
+
+export const App = () => {
+  const [m, setMessage] = useState<Message>({ message: '' });
+
+  useEffect(() => {
+    fetch('/api')
+      .then((r) => r.json())
+      .then(setMessage);
+  }, []);
+
+  return (
+    <>
+      <div style={{ textAlign: 'center' }}>
+        <h1>Welcome to monorepo-test!</h1>
+        <img
+          width="450"
+          src="https://raw.githubusercontent.com/nrwl/nx/master/images/nx-logo.png"
+          alt="Nx - Smart, Fast and Extensible Build System"
+        />
+      </div>
+      <div>{m.message}</div>
+    </>
+  );
+};
+
+export default App;
