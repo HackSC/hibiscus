45e534cc9f369d1d7c3e69e83c2c96aaa469aa68
Vincent Vu <vinceny.fe@gmail.com>
2022-06-11 14:18:08 -0700

    change project name

--- apps/www-2023/jest.config.ts
+++ apps/www-2023/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
-  displayName: 'landing-page',
+  displayName: 'www-2023',
   preset: '../../jest.preset.js',
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
     '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nrwl/next/babel'] }],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../coverage/apps/landing-page',
+  coverageDirectory: '../../coverage/apps/www-2023',
 };
