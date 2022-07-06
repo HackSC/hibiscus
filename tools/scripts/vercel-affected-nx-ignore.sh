fa4096e5baaff8d0ac8f2fbb4aff3e8fa1491ffa
Vincent Vu <vinceny.fe@gmail.com>
2022-07-05 17:37:05 -0700

    fix bash script

--- tools/scripts/vercel-affected-nx-ignore.sh
+++ tools/scripts/vercel-affected-nx-ignore.sh
@@ -2,7 +2,7 @@ APP=$1
 
 # these can often be not pre-installed on the Vercel build server so we install them here
 NX_VER=$(node -e "console.log(require('./package.json').devDependencies['@nrwl/workspace'])")
-TS_VER=$(node -e "console.log(require('./package.json').devDependencies['@nrwl/workspace'])")
+TS_VER=$(node -e "console.log(require('./package.json').devDependencies['typescript'])")
 
 # Install @nrwl/workspace in order to run the affected command
 yarn add --dev @nrwl/workspace@$NX_VER --prefer-offline
