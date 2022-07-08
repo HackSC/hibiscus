f0d0d70d4e9214475c0ea602591d05ce8ff80290
SuhitAgarwal <suhitagarwal@gmail.com>
2022-07-07 22:57:11 -0700

    email newsletter

--- apps/interim/components/email-newsletter-input-section/email-newsletter-input-section.tsx
+++ apps/interim/components/email-newsletter-input-section/email-newsletter-input-section.tsx
@@ -12,53 +12,12 @@ export function EmailNewsletterInputSection(
   };
 
   return (
-    <div
-      style={{
-        display: 'flex',
-        width: '100%',
-        alignItems: 'center',
-        marginBottom: '2rem',
-      }}
-    >
-      <input
-        style={{
-          border: 'solid 0.1rem #BCBCBC',
-          backgroundColor: '#F8F8F8',
-          borderRadius: 10,
-          padding: 10,
-          paddingLeft: 15,
-          fontFamily: 'Inter',
-          fontSize: 15,
-          color: '#676767',
-          marginRight: 5,
-          maxWidth: '100%',
-          width: '100%',
-        }}
-        placeholder={'Subscribe to our email newsletter!'}
-      />
-      <button
-        style={{
-          background: `linear-gradient(90deg, ${TrademarkColors.LIGHT_BLUE} 0%, ${TrademarkColors.LIGHT_PURPLE} 100%)`,
-          border: 'none',
-          padding: '12px 5px',
-          color: 'white',
-          fontWeight: 700,
-          borderRadius: 10,
-          boxShadow: '0px 2px 10px -1px #a0a0a0',
-          fontFamily: 'Inter',
-          fontSize: 15,
-          cursor: 'pointer',
-          minWidth: '30%',
-        }}
-        onClick={submitSubscribeNewsletterHandler}
-      />
-      <Container>
-        <EmailInput placeholder={'Subscribe to our email newsletter!'} />
-        <GradientButton onClick={submitSubscribeNewsletterHandler}>
-          STAY UP TO DATE
-        </GradientButton>
-      </Container>
-    </div>
+    <Container>
+      <EmailInput placeholder={'Subscribe to our email newsletter!'} />
+      <GradientButton onClick={submitSubscribeNewsletterHandler}>
+        STAY UP TO DATE
+      </GradientButton>
+    </Container>
   );
 }
 
@@ -76,6 +35,7 @@ const GradientButton = styled.button({
   border: 'none',
   padding: '12px 2px',
   color: 'white',
+  fontFamily: 'Inter',
   fontWeight: 700,
   borderRadius: 10,
   boxShadow: '0px 2px 10px -1px #a0a0a0',
@@ -90,6 +50,7 @@ const EmailInput = styled.input({
   borderRadius: 10,
   padding: 10,
   paddingLeft: 15,
+  fontFamily: 'Inter',
   fontSize: 15,
   color: '#676767',
   marginRight: 5,
