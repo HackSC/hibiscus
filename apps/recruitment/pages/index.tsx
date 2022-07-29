d83e64d4df342335c229b1cfd666f7ded0640ffd
Vincent Vu <vinceny.fe@gmail.com>
2022-07-28 20:55:58 -0700

    add recruitment page

---
+++ apps/recruitment/pages/index.tsx
@@ -0,0 +1,12 @@
+import { H2 } from '@hacksc-platforms/ui';
+
+export function Index() {
+  /*
+   * Replace the elements below with your own.
+   *
+   * Note: The corresponding styles are in the ./index.styled-components file.
+   */
+  return <H2>Hello</H2>;
+}
+
+export default Index;
