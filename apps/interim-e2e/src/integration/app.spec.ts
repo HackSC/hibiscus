8e94e990cfa8956c3b665f704dad1bd10b89cf53
Vincent Vu <vinceny.fe@gmail.com>
2022-08-05 16:51:10 -0700

    nx remove the e2es (#53)

--- apps/interim-e2e/src/integration/app.spec.ts
+++
@@ -1,8 +0,0 @@
-describe('interim', () => {
-  beforeEach(() => cy.visit('/'));
-
-  it('should display welcome message', () => {
-    // Custom command example, see `../support/commands.ts` file
-    cy.login('my-email@something.com', 'myPassword');
-  });
-});
