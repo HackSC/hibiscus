d6c5fe6e7a26be3e1107146066e9717417b4fcdb
Suhit Agarwal <suhitagarwal@gmail.com>
2022-08-26 18:13:05 -0700

    google analytics update + added on landing page (#117)

--- libs/analytics/src/lib/google-analytics-4-script.tsx
+++ libs/analytics/src/lib/google-analytics-4-script.tsx
@@ -3,25 +3,25 @@ import { useId } from 'react';
 
 /* eslint-disable-next-line */
 export interface AnalyticsProps {
-  streamingId: string;
+  measuringId: string;
 }
 
 /**
- * A component to easily enable GA4 on a Next.js website given its `streamingId`; requires a GA4 property to be created
+ * A component to easily enable GA4 on a Next.js website given its `measuringId`; requires a GA4 property to be created
  */
-export function GoogleAnalytics4Script({ streamingId }: AnalyticsProps) {
+export function GoogleAnalytics4Script({ measuringId }: AnalyticsProps) {
   return (
     <>
       <Script
         strategy="lazyOnload"
-        src={`https://www.googletagmanager.com/gtag/js?id=${streamingId}`}
+        src={`https://www.googletagmanager.com/gtag/js?id=${measuringId}`}
       />
       <Script strategy="lazyOnload" id={useId()}>
         {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', '${streamingId}', {
+            gtag('config', '${measuringId}', {
               page_path: window.location.pathname,
             });
                 `}
