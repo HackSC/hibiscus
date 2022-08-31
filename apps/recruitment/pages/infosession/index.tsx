b1d06040a70756c48312a5bebf3c4f93392141b3
Vincent Vu <vinceny.fe@gmail.com>
2022-08-31 13:48:48 -0700

    done

---
+++ apps/recruitment/pages/infosession/index.tsx
@@ -0,0 +1,20 @@
+import { GetServerSideProps } from 'next';
+
+/* eslint-disable-next-line */
+export interface InfoSessionRedirectProps {}
+
+export function InfoSessionRedirect(props: InfoSessionRedirectProps) {
+  return <div className="main"></div>;
+}
+
+export default InfoSessionRedirect;
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const zoomInfoSessionURL = process.env.ZOOM_INFO_SESSION_URL;
+  return {
+    redirect: {
+      destination: zoomInfoSessionURL,
+      permanent: false,
+    },
+  };
+};
