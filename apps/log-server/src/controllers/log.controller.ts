bd841b782ff7082effc42aace3f04199ce81f084
Jiong Yan Yap <jiongyan@usc.edu>
2022-11-02 23:14:07 -0700

    Fix log server controller layer (#183)
    * Fix controller to compile, fix dependency injection
    
    * Use express routers to organize routes
    
    * Change API routes for get and create logs to /type/:type/log
    
    * Add API endpoint /type/ for adding schema to database

--- apps/log-server/src/controllers/log.controller.ts
+++ apps/log-server/src/controllers/log.controller.ts
@@ -1,16 +1,18 @@
-import { installPackagesTask } from '@nrwl/devkit';
 import * as express from 'express';
-import { container, injectable } from 'tsyringe';
+import { injectable } from 'tsyringe';
 import { LogRepository } from '../repositories/log.repository';
 
 @injectable()
-class LogController {
+export class LogController {
   /**
    * Setup constructor
    */
+  constructor(private userRepo: LogRepository) {
+    this.userRepo = userRepo;
+  }
+
   async initialize() {
-    const userRepo = container.resolve(LogRepository);
-    await userRepo.initDB();
+    await this.userRepo.initDb();
   }
 
   /**
@@ -19,15 +21,14 @@ class LogController {
    * @param res - outgoing response
    */
   async createLog(req: express.Request, res: express.Response) {
-    const userRepo = container.resolve(LogRepository);
     // schema validation step
 
     // call repo method
     const timeOfLogCreation = Date.now();
     try {
       const log = req.body.log;
-      const type = req.body.type;
-      await userRepo.insertLog(log, type, timeOfLogCreation);
+      const type = req.params.type;
+      await this.userRepo.insertLog(log, type, timeOfLogCreation);
       res.status(200).json({ message: 'New log added successfully' });
     } catch (err) {
       res.status(500).json({ message: err.message });
@@ -40,16 +41,20 @@ class LogController {
    * @param res outgoing response (list of logs)
    */
   async getLog(req: express.Request, res: express.Response) {
-    const userRepo = container.resolve(LogRepository);
     // schema validation step
 
     // call repo method
     try {
-      const type = req.query.type;
-      const query = req.query.query;
-      const sortMethod = req.query.sortMethod;
-      const page = req.query.page;
-      const log = await userRepo.getLogs(type, query, sortMethod, page);
+      const type = req.params.type;
+      const query = req.body.query;
+      const sortMethod = req.body.sortMethod;
+      const page = req.body.page;
+      const log = await this.userRepo.getLogs({
+        type,
+        query,
+        sortMethod,
+        page,
+      });
       res.status(200).json(log);
     } catch (err) {
       res.status(500).json({ message: err.message });
@@ -62,16 +67,13 @@ class LogController {
    * @param res outgoing request
    */
   async createSchema(req: express.Request, res: express.Response) {
-    const userRepo = container.resolve(LogRepository);
     try {
       const type = req.body.type;
       const schema = req.body.schema;
-      const log = await userRepo.insertSchema(type, schema);
-      res.status(200).json(log);
+      await this.userRepo.insertSchema(type, schema);
+      res.status(200).json({ message: 'New schema added successfully' });
     } catch (err) {
       res.status(500).json({ message: err.message });
     }
   }
 }
-
-export default new LogController();
