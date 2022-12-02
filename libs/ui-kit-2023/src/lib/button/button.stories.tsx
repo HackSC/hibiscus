7886cbb9109cd298063ef8fe3244bfb95f635c40
Vincent Vu <vinceny.fe@gmail.com>
2022-12-02 15:39:54 -0800

    don't know why index.tsx was lost but ok

--- libs/ui-kit-2023/src/lib/button/button.stories.tsx
+++ libs/ui-kit-2023/src/lib/button/button.stories.tsx
@@ -1,4 +1,5 @@
 import type { ComponentStory, ComponentMeta } from '@storybook/react';
+import React from 'react';
 import { Button } from './button';
 
 const Story: ComponentMeta<typeof Button> = {
--- libs/ui-kit-2023/src/lib/one-line-text/one-line-text.tsx
+++ libs/ui-kit-2023/src/lib/one-line-text/one-line-text.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from 'react';
+import { useRef } from 'react';
 import styled from 'styled-components';
 
 /* eslint-disable-next-line */
