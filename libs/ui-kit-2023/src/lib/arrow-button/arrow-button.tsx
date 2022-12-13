65315a6107c55908a1acc5f5870279c633421f4d
Vincent Vu <vmvu@usc.edu>
2022-12-13 13:50:29 -0800

    implemented arrow button (#214)

---
+++ libs/ui-kit-2023/src/lib/arrow-button/arrow-button.tsx
@@ -0,0 +1,52 @@
+import { Colors2023 } from '@hacksc-platforms/styles';
+import styled from 'styled-components';
+import Image from 'next/image';
+import arrow from '../../assets/chevron-down.svg';
+import React from 'react';
+
+/* eslint-disable-next-line */
+export interface ArrowButtonProps extends React.ButtonHTMLAttributes<any> {
+  orientation?: 'right' | 'left' | 'up' | 'down';
+}
+
+export function ArrowButton(props: ArrowButtonProps) {
+  return (
+    <StyledArrowButton {...props} orientation={props.orientation}>
+      <Image
+        src={arrow}
+        alt={`Arrow if pressed will go to the ${
+          props.orientation === 'right' || props.orientation === 'down'
+            ? 'next'
+            : 'previous'
+        } question`}
+        width={20}
+        height={20}
+      />
+    </StyledArrowButton>
+  );
+}
+
+export default ArrowButton;
+
+const StyledArrowButton = styled.button<{
+  orientation: ArrowButtonProps['orientation'];
+}>`
+  transform: ${({ orientation }) =>
+    orientation === 'left'
+      ? 'rotate(180deg)'
+      : orientation === 'down'
+      ? 'rotate(90deg)'
+      : orientation === 'up'
+      ? 'rotate(-90deg)'
+      : 'rotate(0)'};
+  background: transparent;
+  color: ${Colors2023.BLUE_STANDARD};
+  border: 1.5px solid ${Colors2023.BLUE_STANDARD};
+  border-radius: 8px;
+  padding: 8px
+    ${({ orientation }) =>
+      orientation === 'up' || orientation === 'down' ? '8px' : '9px'};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
