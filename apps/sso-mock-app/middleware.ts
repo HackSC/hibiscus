c81237760f71d3ac83dd876103a98200f8a33c71
Jiong Yan Yap <jiongyan@usc.edu>
2022-12-23 15:37:17 -0800

    Guard API route if not accessed by SSO domain

--- apps/sso-mock-app/middleware.ts
+++ apps/sso-mock-app/middleware.ts
@@ -2,6 +2,19 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 export async function middleware(request: NextRequest) {
+  // Guard API route
+  const path = request.nextUrl.pathname.split('/');
+  if (path.length >= 2 && path[1] === 'api') {
+    if (
+      request.method !== 'GET' &&
+      request.headers.get('origin') === process.env.SSO_URL
+    ) {
+      return NextResponse.next();
+    } else {
+      return NextResponse.redirect(new URL('/', request.url));
+    }
+  }
+
   if (request.cookies.has(process.env.HIBISCUS_COOKIE_NAME)) {
     const token = request.cookies.get(process.env.HIBISCUS_COOKIE_NAME);
     const res = await fetch(`${process.env.SSO_URL}/api/verifyToken`, {
@@ -32,6 +45,6 @@ export const config = {
      * - _next/static (static files)
      * - favicon.ico (favicon file)
      */
-    '/((?!api|_next/static|favicon.ico).*)',
+    '/((?!_next/static|favicon.ico).*)',
   ],
 };
