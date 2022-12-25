69cfa6281cbe90a5971220c2e9f2a8b2fc8c097e
Vincent Vu <vmvu@usc.edu>
2022-12-24 22:56:09 -0800

    skip build if app doesnt exist in branch (#245)
    * modify script
    
    * scanning workspace.json makes more sense

--- tools/scripts/vercel-affected-nx-ignore.sh
+++ tools/scripts/vercel-affected-nx-ignore.sh
@@ -4,7 +4,17 @@ set -e
 
 APP=$1
 
-# these can often be not pre-installed on the Vercel build server so we install them here
+# check if app in branch filesystem; if so then proceed
+echo "Checking if $APP is in source filesystem"
+APP_IN_WORKSPACEJSON=$(node -e "console.log(require('./workspace.json').projects['$APP']!==undefined)")
+echo "App is in workspace.json: $APP_IN_WORKSPACEJSON"
+if [ "$APP_IN_WORKSPACEJSON" == "false" ]; then
+	echo "App $APP doesn't exist in this checkout's filesystem; skipping build..."
+	exit 0;
+fi
+echo "App $APP is in filesystem; continuing build..."
+
+# # these can often be not pre-installed on the Vercel build server so we install them here
 NX_VER=$(node -e "console.log(require('./package.json').devDependencies['@nrwl/workspace'])")
 TS_VER=$(node -e "console.log(require('./package.json').devDependencies['typescript'])")
 
