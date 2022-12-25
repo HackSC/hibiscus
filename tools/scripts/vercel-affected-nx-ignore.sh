16709fb2f91dc676f9b5b6382248ff61e5731bed
Vincent <vinceny.fe@gmail.com>
2022-12-24 21:12:37 -0800

    modify script

--- tools/scripts/vercel-affected-nx-ignore.sh
+++ tools/scripts/vercel-affected-nx-ignore.sh
@@ -4,6 +4,12 @@ set -e
 
 APP=$1
 
+# check if app in branch filesystem; if so then proceed
+if [ ! -d "./apps/$APP" ]; then
+	echo "App $APP doesn't exist in this checkout's filesystem; skipping build..."
+	exit 0;
+fi
+
 # these can often be not pre-installed on the Vercel build server so we install them here
 NX_VER=$(node -e "console.log(require('./package.json').devDependencies['@nrwl/workspace'])")
 TS_VER=$(node -e "console.log(require('./package.json').devDependencies['typescript'])")
