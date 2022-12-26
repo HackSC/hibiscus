3000428f59351a1287b0f6cffe2b998c9c7f31ed
Benran Zhang <jimyzhan@usc.edu>
2022-12-26 14:38:07 -0800

    additional functionalities

--- apps/dashboard/pages/api/invitee.ts
+++ apps/dashboard/pages/api/invitee.ts
@@ -16,19 +16,51 @@ async function inviteeAccept(req, res) {
     //response with internal server error
     res.status(500).json({ error_message: error.message });
   } else {
-    res
-      .status(200)
-      .json({ success_message: 'successfully accepted invitation' });
-    //code for email notification
+    //delete the request in the db
+    const { error } = await supabase
+      .from('invitations')
+      .update({ status: 1 })
+      .eq('id', req.invitation_id);
+    if (error != null) {
+      console.log(error.message);
+    } else {
+      res
+        .status(200)
+        .json({ success_message: 'successfully accepted invitation' });
+      //code for email notification
+    }
   }
 }
 
 async function inviteeDeny(req, res) {
   //just send email notification
+  //delete invitation record
+  const { error } = await supabase
+    .from('invitations')
+    .update({ status: -1 })
+    .eq('id', req.invitation_id);
+  if (error != null) {
+    console.log(error.message);
+  } else {
+    res.status(200).json({ success_message: 'successfully denied invitation' });
+  }
 }
 
 async function inviteeRequest(req, res) {
   //send email to organizer
+  const { error } = await supabase.from('requests').insert({
+    hacker_id: req.hacker_id,
+    organizer_id: req.organizer_id,
+    team_id: req.team_id,
+    status: 0,
+  });
+
+  if (error != null) {
+    console.log(error.message);
+    res.status(500).json({ error_message: error.message });
+  } else {
+    res.status(200).json({ success_message: 'successfully sent request' });
+  }
 }
 
 export default { inviteeAccept, inviteeDeny, inviteeRequest };
