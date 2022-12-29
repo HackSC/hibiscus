167899c3445935e1784b5985985bfb73b1638d2e
Anna Hsu <annaddhsu@gmail.com>
2022-12-28 21:47:17 -0500

    fix input flow to match design system and add transition

--- libs/ui-kit-2023/src/lib/paragraph-text/paragraph-text.tsx
+++ libs/ui-kit-2023/src/lib/paragraph-text/paragraph-text.tsx
@@ -33,7 +33,8 @@ const Input = styled.textarea`
 
   color: #f4f4f4;
   :hover {
-    filter: drop-shadow(0px 0px 15px #c2c2c2);
+    filter: drop-shadow(0px 0px 3px #c2c2c2);
+    transition: 0.1s;
   }
   :focus {
     /* gray/medium */
