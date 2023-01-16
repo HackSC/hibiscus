3d5f83fdc4f3d8066876b234498135e652408607
Vincent <vinceny.fe@gmail.com>
2023-01-15 20:27:26 -0800

    add rough implementation

--- apps/event-service/app.py
+++ apps/event-service/app.py
@@ -1,9 +1,10 @@
 import os
-from flask import Flask
-from dotenv import load_dotenv
+from flask import Flask, request
+from dotenv import load_dotenv, find_dotenv
 from supabase import create_client
 
 load_dotenv()
+find_dotenv()
 
 app = Flask(__name__)
 
@@ -11,20 +12,26 @@ supabase_api_url = os.getenv("NEXT_PUBLIC_HIBISCUS_SUPABASE_API_URL")
 supabase_service_key = os.getenv("HIBISCUS_SUPABASE_SERVICE_KEY")
 supabase = create_client(supabase_api_url, supabase_service_key)
 
+# TODO: configure CORs
+
 @app.get("/events")
 def get_events():
-    pass
+    res = supabase.table("events").select("name,description,start,end,id").execute();
+    return res.data
 
 @app.get("/events/<user_id>/pins")
 def get_pinned_events(user_id: str):
-    pass
+    res = supabase.table("pinned_events").select("events(name, description, start, end)").eq("user_id", user_id).execute();
+    return res.data
 
-@app.put("/events/<user_id>/pin")
-def pin_event(user_id: str):
-    pass
+@app.post("/events/<user_id>/pins/<event_id>")
+def pin_event(user_id: str, event_id: int):
+    res = supabase.table("pinned_events").insert({"user_id": user_id, "event_id": event_id}).execute()
+    return res.data
 
-@app.delete("/events/<user_id>/pin")
-def remove_pin(user_id: str):
-    pass
+@app.delete("/events/<user_id>/pins/<event_id>")
+def remove_pin(user_id: str, event_id: int):
+    res = supabase.table("pinned_events").delete().eq("user_id", user_id).eq("event_id", event_id).execute()
+    return res.data
 
 # feat: when event gets pinned and it's near, then it should notify the user via DM
