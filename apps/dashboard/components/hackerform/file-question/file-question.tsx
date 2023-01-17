0f0e4c3be35182a586112dc6d6d415404482d1f0
Vincent Vu <vinceny.fe@gmail.com>
2023-01-16 18:48:32 -0800

    implemented improvements

--- apps/dashboard/components/hackerform/file-question/file-question.tsx
+++ apps/dashboard/components/hackerform/file-question/file-question.tsx
@@ -1,27 +1,57 @@
 import APIService from '../../../common/api';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import { GetInputResponseCb } from '../../../common/types';
 import QuestionCreator from '../question-creator/question-creator';
 import { ALLOWED_RESUME_FORMATS } from '../../../common/constants';
 import mime from 'mime-types';
 import { useHackform } from '../../../hooks/use-hackform/use-hackform';
 import useHibiscusUser from '../../../hooks/use-hibiscus-user/use-hibiscus-user';
+import { Button } from '@hibiscus/ui-kit-2023';
+import styled from 'styled-components';
+import { Text } from '@hibiscus/ui';
+import { Colors2023 } from '@hibiscus/styles';
+import { ImCross } from 'react-icons/im';
 
 export const FileQuestion = () => {
   const { user } = useHibiscusUser();
-  const hackformUtils = useHackform();
+  const { currentQuestionIndex: cqi, ...hackformUtils } = useHackform();
+  const lastInput = hackformUtils.getCurrentResponse()?.input;
   const [uploaded, setUploaded] = useState<File | null>(null);
-  const [fileKey, setKey] = useState<string | null>(null);
+  const [displayName, setDisplayName] = useState<string | null>(
+    lastInput?.file.displayName ?? null
+  );
+  const [fileKey, setKey] = useState<string | null>(lastInput?.file.fileKey);
+  const fileInputRef = useRef<HTMLInputElement | null>(null);
+  const MAX_FILE_SIZE = 5120; // 5MB
+  const MAX_FILE_SIZE_MB = 5120 / 1024; // 5MB
+
+  const isFileInSize = (file: File) => {
+    const fileSizeKB = file.size / 1024;
+    return fileSizeKB <= MAX_FILE_SIZE;
+  };
 
   const getInputResponse: GetInputResponseCb = () => ({
-    file: { displayName: uploaded?.name, fileKey },
+    file: { displayName, fileKey },
   });
 
-  const handleFileChange: React.ChangeEventHandler<HTMLInputElement> = async (
-    e
-  ) => {
-    const file = e.target.files.item(0);
+  const handleFileChange = async () => {
+    fileInputRef.current.click();
+    const file = fileInputRef.current.files.item(0);
     setUploaded(file);
+    if (!file) {
+      setUploaded(null);
+      return;
+    }
+    if (!isFileInSize(file)) {
+      hackformUtils.addErrorForQuestion(
+        cqi,
+        `Please upload a file size below ${Math.trunc(MAX_FILE_SIZE_MB)} MB`
+      );
+      setUploaded(null);
+      return;
+    }
+    hackformUtils.resolveError(cqi);
+    setDisplayName(file.name);
     const fk = APIService.createFileKey(file);
     setKey(fk);
   };
@@ -44,12 +74,43 @@ export const FileQuestion = () => {
     cb();
   };
 
+  const handleRemoveFile = () => {
+    setUploaded(null);
+    setDisplayName(null);
+    setKey(null);
+  };
+
   const InputComponent = (
-    <input
-      type="file"
-      onChange={handleFileChange}
-      accept={ALLOWED_RESUME_FORMATS.map((it) => mime.lookup(it)).join(',')}
-    />
+    <Container>
+      <FileQuestionContainer>
+        <input
+          ref={fileInputRef}
+          type="file"
+          onChange={handleFileChange}
+          accept={ALLOWED_RESUME_FORMATS.map((it) => mime.lookup(it)).join(',')}
+          hidden
+        />
+        <Button color="blue" onClick={handleFileChange}>
+          UPLOAD
+        </Button>
+        {displayName && (
+          <>
+            <Text>{displayName}</Text>{' '}
+            <button
+              style={{
+                appearance: 'none',
+                background: 'none',
+                cursor: 'pointer',
+              }}
+              onClick={handleRemoveFile}
+            >
+              <ImCross color="red" />
+            </button>
+          </>
+        )}
+      </FileQuestionContainer>
+      <SmallTextGray>Max size: {Math.trunc(MAX_FILE_SIZE_MB)} MB</SmallTextGray>
+    </Container>
   );
 
   return (
@@ -64,3 +125,20 @@ export const FileQuestion = () => {
 };
 
 export default FileQuestion;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`;
+
+const FileQuestionContainer = styled.div`
+  display: flex;
+  gap: 10px;
+  align-items: center;
+`;
+
+const SmallTextGray = styled(Text)`
+  font-size: 12px;
+  color: ${Colors2023.GRAY.SCHEMDIUM};
+`;
