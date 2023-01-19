942ea2f9e2bc92257fb8cf13e93eb0b467f9683b
Suhit Agarwal <suhitagarwal@gmail.com>
2023-01-19 12:37:28 -0800

    responsive button styling w.r.t. upcoming hero background based on conventional breakpoints and some custom breakpoints as well

--- apps/www-2023/components/apply-now/apply-now.tsx
+++ apps/www-2023/components/apply-now/apply-now.tsx
@@ -10,22 +10,40 @@ const StyledApplyNow = styled.div`
   position: absolute;
   top: 60vh;
   left: 59vw;
+  @media screen and (max-width: 1600px) {
+    top: 57vh;
+    left: 56vw;
+  }
+  @media screen and (max-width: 1280px) {
+    top: 60vh;
+    left: 42vw;
+  }
+  @media screen and (max-width: 768px) {
+    top: 62vh;
+    left: 35vw;
+  }
+  @media screen and (max-width: 400px) {
+    top: 63vh;
+    left: 29vw;
+  }
+  @media screen and (max-width: 340px) {
+    top: 63vh;
+    left: 24vw;
+  }
+  @media screen and (max-width: 290px) {
+    top: 63vh;
+    left: 21vw;
+  }
 `;
 
 export function ApplyNow(props: ApplyNowProps) {
   return (
     <StyledApplyNow>
       <Link href={'https://dashboard.hacksc.com/apply-2023'}>
-        <ApplyNowButton color="black">APPLY NOW</ApplyNowButton>
+        <Button color="black">APPLY NOW</Button>
       </Link>
     </StyledApplyNow>
   );
 }
 
 export default ApplyNow;
-
-const ApplyNowButton = styled(Button)`
-  position: absolute;
-  top: -50vh;
-  left: 50vw;
-`;
