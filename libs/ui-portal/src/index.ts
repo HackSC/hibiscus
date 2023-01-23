376d1a1063c9b7cb65017d88590ec2a0e51b211b
Jiong Yan Yap <jiongyan@usc.edu>
2023-01-22 18:05:20 -0800

    Delete redundant files

--- libs/ui-portal/src/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './lib/layouts/portal-layout';
-export * from './lib/components/portal-menu/portal-menu';
-export * from './lib/components/top-bar/top-bar';
-export * from './lib/hooks/use-hibiscus-user/use-hibiscus-user';
-export * from './lib/store/menu-store';
--- libs/ui-portal/src/lib/common/role.utils.ts
+++
@@ -1,6 +0,0 @@
-import { Colors2023 } from '@hibiscus/styles';
-import { HibiscusRole } from '@hibiscus/types';
-
-export const getColorsForRole = (role: HibiscusRole) => {
-  return Colors2023.roleColors[role];
-};
