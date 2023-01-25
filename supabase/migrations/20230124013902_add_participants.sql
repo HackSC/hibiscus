be1128939af272f96804f858637961aa8ea299c8
Ben Hwang <benhwang@usc.edu>
2023-01-24 19:14:48 -0800

    Make migrations for participants and WIP migration for notes.

---
+++ supabase/migrations/20230124013902_add_participants.sql
@@ -0,0 +1,25 @@
+create table "public"."participants" (
+    "id" uuid not null,
+    "created_at" timestamp with time zone default now(),
+    "first_name" character varying,
+    "last_name" character varying,
+    "DOB" character varying,
+    "school" character varying,
+    "major" character varying,
+    "graduation_year" character varying,
+    "resume" character varying,
+    "portfolio_link" character varying
+);
+
+
+alter table "public"."participants" enable row level security;
+
+CREATE UNIQUE INDEX participants_pkey ON public.participants USING btree (id);
+
+alter table "public"."participants" add constraint "participants_pkey" PRIMARY KEY using index "participants_pkey";
+
+alter table "public"."participants" add constraint "participants_id_fkey" FOREIGN KEY (id) REFERENCES user_profiles(user_id) not valid;
+
+alter table "public"."participants" validate constraint "participants_id_fkey";
+
+
