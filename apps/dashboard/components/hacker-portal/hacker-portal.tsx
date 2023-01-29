f59ad8af0d659a5e599b7dfed3e66e2eb4a58864
Vincent <vinceny.fe@gmail.com>
2023-01-28 17:29:43 -0800

    small fixes and migration

--- apps/dashboard/components/hacker-portal/hacker-portal.tsx
+++ apps/dashboard/components/hacker-portal/hacker-portal.tsx
@@ -16,6 +16,7 @@ import { GrayBox } from '../gray-box/gray-box';
 import { container } from 'tsyringe';
 import { HibiscusSupabaseClient } from '@hibiscus/hibiscus-supabase-client';
 import DeclinedPlaceholder from './declined-placeholder';
+import { CongratsMessage } from './congrats-message';
 
 type RSVPChoice = 'DECLINE' | 'ACCEPT';
 
@@ -125,48 +126,7 @@ export function HackerPortal() {
   const RSVPPlaceholder = () => {
     return (
       <div style={{ display: 'flex', flexDirection: 'column', gap: '10px' }}>
-        <Image
-          src="/hackform-illustrations/heart.svg"
-          width={100}
-          height={100}
-          alt="Heart character"
-        />
-        <H3>Congratulations on being accepted to HackSC 2023!</H3>
-        <Text>
-          We are so excited to welcome you to our event this year, and hope that
-          you will be able to attend. It’s going to be an amazing weekend with
-          plenty of workshops, speakers, games, and of course, project building!
-        </Text>
-        <Text>
-          Please complete this RSVP form by{' '}
-          <GlowSpan style={{ fontWeight: 'bold' }}>
-            February 1st, 2023 at 11:59pm
-          </GlowSpan>{' '}
-          to secure your spot at HackSC 2023. By submitting, you are committing
-          to attending the event. If you do not submit this form by the posted
-          deadline, your spot will be given to another hacker.
-        </Text>
-        <Text>
-          We kindly remind you that no-shows after RSVPing mean that you are
-          taking someone else’s opportunity to attend this event. If you are no
-          longer able to attend the event after RSVPing, please email{' '}
-          <Link
-            href="mailto:team@hacksc.com"
-            passHref
-            underline
-            anchortagpropsoverride={{
-              style: { color: Colors2023.BLUE.STANDARD },
-            }}
-          >
-            team@hacksc.com
-          </Link>{' '}
-          at least 12 hours prior to the start of the event.
-        </Text>
-        <Text>
-          HackSC is committed to providing participants with the best experience
-          possible. If there’s anything we can do to improve your time at our
-          event, please let us know!
-        </Text>
+        <CongratsMessage />
         <div style={{ display: 'flex', gap: '10px' }}>
           <Button
             color="red"
@@ -304,7 +264,7 @@ const MessageContainer = styled.div`
   justify-content: center;
   align-items: center;
   gap: 5px;
-  padding-top: 100px;
+  padding-top: 50px;
 `;
 
 const BannerContainer = styled.div`
--- supabase/migrations/20230126033646_rsvp_form_modifications.sql
+++ supabase/migrations/20230126033646_rsvp_form_modifications.sql
@@ -2,4 +2,6 @@ alter table "public"."participants" disable row level security;
 
 alter table "public"."user_profiles" add column "attendance_confirmed" boolean;
 
+alter table "public"."participants" drop constraint "participants_resume_fkey";
 
+alter table "public"."participants" alter column "resume" set data type character varying using "resume"::character varying;
