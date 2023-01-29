d0df661a286d54d56ffb75ef29b7298abc301a86
Vincent <vinceny.fe@gmail.com>
2023-01-29 01:29:44 -0800

    add sentry

--- apps/dashboard/middleware.ts
+++ apps/dashboard/middleware.ts
@@ -5,6 +5,7 @@ import { TSRV_RELEASE_FLAG } from './common/constants';
 
 export async function middleware(request: NextRequest) {
   // Intercept requests to application page and redirect to signup page if no session detected
+
   const res = await middlewareHandler(
     `${getEnv().Hibiscus.AppURL.portal}/api/callback`,
     ['/apply-2023'],
