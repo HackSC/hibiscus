9bf9cff879c7e97a2b230f3b6ed44ac14f12e40f
Vincent Vu <vinceny.fe@gmail.com>
2023-01-29 16:24:16 -0800

    fix capitalized column name

--- supabase/migrations/20230130001948_move_column_wristband_id_to_participants.sql
+++ supabase/migrations/20230130001948_move_column_wristband_id_to_participants.sql
@@ -1,9 +1,9 @@
-alter table "public"."participants" add column "wristband_Id" character varying;
+alter table "public"."participants" add column "wristband_id" character varying;
 
 alter table "public"."user_profiles" drop column "wristband_id";
 
-CREATE UNIQUE INDEX "participants_wristband_Id_key" ON public.participants USING btree ("wristband_Id");
+CREATE UNIQUE INDEX "participants_wristband_id_key" ON public.participants USING btree ("wristband_id");
 
-alter table "public"."participants" add constraint "participants_wristband_Id_key" UNIQUE using index "participants_wristband_Id_key";
+alter table "public"."participants" add constraint "participants_wristband_id_key" UNIQUE using index "participants_wristband_id_key";
 
 
