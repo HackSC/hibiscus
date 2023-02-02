ad450c560786d0a624f68c7470487a1853820c7f
Terry Chen <chenterry85@gmail.com>
2023-02-01 17:52:02 -0800

    Added: get_sorted_events_by_day

--- apps/event-service/app.py
+++ apps/event-service/app.py
@@ -27,6 +27,35 @@ def get_events():
         "name,description,start,end,id").execute()
     return res.data
   
+@app.get("/events/<user_id>/<day>")
+def get_sorted_events_by_day(user_id: str, day: int):
+  day_begin = datetime(2023,2,3) + timedelta(int(day) - 1)
+  day_end = day_begin + timedelta(1)
+  
+  # fetch pinned events
+  pinned_events = get_pinned_events(user_id)
+    
+  # filter pinned events by day
+  pinned_events = list(filter(lambda event: day_begin.isoformat('T','auto') < event["events"]["start"] < day_end.isoformat('T','auto'), pinned_events))
+  
+  # sort pinned events by start time
+  pinned_events.sort(key = lambda x: datetime.strptime(x["events"]["start"], "%Y-%m-%dT%H:%M:%S%z"))
+
+  # gather id of all pinned events 
+  pinned_events_id = set()
+  for e in pinned_events:
+    pinned_events_id.add(e["events"]["id"])
+  
+  # fetch all events sorted by day
+  all_events = get_events_by_day(day)
+  
+  # find all non-pinned events
+  non_pinned_events = []
+  for event in all_events:
+    if event["id"] not in pinned_events_id:
+      non_pinned_events.append(event)
+    
+  return pinned_events + non_pinned_events 
   
 @app.get("/events/<day>")
 def get_events_by_day(day: int):
@@ -40,7 +69,7 @@ def get_events_by_day(day: int):
 @app.get("/events/<user_id>/pins")
 def get_pinned_events(user_id: str):
     res = supabase.table("pinned_events").select(
-        "events(name, description, start, end)").eq("user_id",
+        "events(id, name, description, start, end)").eq("user_id",
                                                     user_id).execute()
     return res.data
 
