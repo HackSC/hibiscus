347b1e11b508aa66f5d4cb36154d3bbafd4738d0
Vincent Vu <vinceny.fe@gmail.com>
2023-03-03 17:23:06 -0800

    made full hacker apps get script

--- apps/airplane/tasks/get_2023_hackers.py
+++ apps/airplane/tasks/get_2023_hackers.py
@@ -3,28 +3,17 @@ from supabase.client import create_client
 import boto3
 import time
 
-
-# This is your task's entrypoint. When your task is executed, this
-# function will be called.
-def main(params):
-    api_url = os.getenv("NEXT_PUBLIC_HIBISCUS_SUPABASE_API_URL") or ""
-    service_key = os.getenv("HIBISCUS_SUPABASE_SERVICE_KEY") or ""
-    client = create_client(api_url, service_key)
+def get_hacker_apps_in_batches(hackers: list, pull_batch_size: int):
     dynamo = boto3.resource(
         "dynamodb",
         region_name="us-west-1",
         aws_access_key_id=os.getenv("HIBISCUS_AWS_ACCESS_KEY_ID"),
         aws_secret_access_key=os.getenv("HIBISCUS_AWS_SECRET_ACCESS_KEY"),
     )
-    res = client.table("user_profiles").select("*").execute()
-    hackers = res.data[:500]
-    hackers = list(filter(lambda x: x["app_id"] is not None, hackers))
-    # get resume data in batches of 10
-    batch_size = 80
-    for i in range(0, len(hackers), batch_size):
+    for i in range(0, len(hackers), pull_batch_size):
         if i >= len(hackers):
             break
-        batch = hackers[i : i + batch_size]
+        batch = hackers[i : i + pull_batch_size]
         app_id_objects = list(map(lambda x: {"id": x["app_id"]}, batch))
         response = dynamo.batch_get_item(
             RequestItems={
@@ -36,13 +25,30 @@ def main(params):
             ReturnConsumedCapacity="TOTAL",
         )
         responses = response["Responses"]["hacker-app-responses"]
-        for j in range(i, i + batch_size):
+        for j in range(i, i + pull_batch_size):
             if j >= len(hackers):
                 break
             for rep in responses:
                 if rep["id"] == hackers[j]["app_id"]:
                     hackers[j]["application_json"] = rep["data"]
-        time.sleep(1)
-    with open("hackers.txt", "w") as f:
-        f.write(str(hackers))
-    return f"{len(hackers)} json files modified!"
+
+# This is your task's entrypoint. When your task is executed, this
+# function will be called.
+def main(params):
+    api_url = os.getenv("NEXT_PUBLIC_HIBISCUS_SUPABASE_API_URL") or ""
+    service_key = os.getenv("HIBISCUS_SUPABASE_SERVICE_KEY") or ""
+    client = create_client(api_url, service_key)
+    res = client.table("user_profiles").select("*").execute()
+    # split the total user profiles, do them in 300s
+    agg_list_batch = 300
+    pull_batch_size = 80
+    agg_list = []
+    for i in range(0, len(res.data), agg_list_batch):
+        hackers = res.data[i:i+agg_list_batch]
+        hackers = list(filter(lambda x: x["app_id"] is not None, hackers))
+        # pull apps data from ext source in batches of 80s
+        get_hacker_apps_in_batches(hackers, pull_batch_size)
+        agg_list += hackers.copy()
+    with open("hackers.json", "w") as f:
+        f.write(str({'data': agg_list}))
+    return f"{len(agg_list)} json files modified!"
