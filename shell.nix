667873ff35dde61c3dc92bea402c148abd3716d3
Vincent Vu <vinceny.fe@gmail.com>
2023-03-22 15:41:50 -0700

    add shell nix

---
+++ shell.nix
@@ -0,0 +1,19 @@
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/06278c77b5d162e62df170fec307e83f1812d94b.tar.gz") { } }:
+
+pkgs.mkShell {
+  buildInputs = [
+    pkgs.which
+    pkgs.htop
+    pkgs.zlib
+    pkgs.nodejs-slim-16_x
+    (pkgs.python39.withPackages (ps: [
+      ps.flask
+      ps.flake8
+      ps.black
+    ]))
+    pkgs.python39Packages.poetry
+    pkgs.python39Packages.pre-commit-hooks
+    pkgs.yarn
+    pkgs.docker
+  ];
+}
