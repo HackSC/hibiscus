a0f0b59fda03398e8d93791be2e4da191b49bb3b
Vincent Vu <vinceny.fe@gmail.com>
2023-03-28 00:55:43 -0700

    delete mock app

--- apps/sso-mock-app/pages/api/callback.ts
+++
@@ -1,8 +0,0 @@
-import { NextApiHandler } from 'next';
-import { callbackApiHandler } from '@hibiscus/sso-client';
-
-const handler: NextApiHandler = callbackApiHandler(
-  process.env.NEXT_PUBLIC_SSO_MOCK_APP_URL
-);
-
-export default handler;
