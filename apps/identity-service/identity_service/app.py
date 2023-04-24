d522e059f7a9e33a16a1997a6e6ac5ebba6b9017
Jordan Sefah <js986@njit.edu>
2023-04-23 18:17:04 -0700

    rewrite endpoints based on new routes

--- apps/identity-service/identity_service/app.py
+++ apps/identity-service/identity_service/app.py
@@ -43,53 +43,104 @@ def get_participants():
 
 @app.get("/participants/<pid>")
 def get_participant(pid):
-    data = supabase.table("participants").select("*").eq("id", pid).execute()
+    participant_data = (
+        supabase.table("participants").select("*").eq("id", pid).execute()
+    )
+    events_attended = []
+    log_data = supabase.table("event_logs").select("*").eq("user_id", pid).execute()
+    user_data = supabase.table("user_profiles").select("*").eq("id", pid).execute()
+    user_name = user_data.data["first_name"] + " " + user_data.data["last_name"]
+    for d in log_data.data:
+        event_data = (
+            supabase.table("events").select("*").eq("id", d["user_id"]).execute()
+        )
+        events_attended.append(event_data.data)
+
+    return jsonify(
+        {
+            "participant_id": pid,
+            "name": user_name,
+            "school": participant_data.data["school"],
+            "major": participant_data.data["major"],
+            "year": participant_data.data["graduation_year"],
+            "events_attended": events_attended,
+        }
+    )
+
     return jsonify(data.data)
+
+
+@app.get("/participants/logs/<log_id>")
+def get_log_id(log_id):
+    log = supabase.table("event_log").select("*").eq("id", log_id).execute()
+
+    user_id = log.data["user_id"]
+    event_id = log.data["event_id"]
+
+    event = supabase.table("events").select("*").eq("id", event_id).execute()
+
+    event_name = event.data["name"]
+
+    user = supabase.table("user_profiles").select("*").eq("id", user_id).execute()
+
+    user_name = user.data["first_name"] + " " + user.data["last_name"]
+
+    return jsonify(
+        {
+            "pid": user_id,
+            "participant-name": user_name,
+            "event-id": event_id,
+            "event-name": event_name,
+            "check-in-time": log.data["check_in_time"],
+        }
+    )
 
 
 @app.post("/participants")
 def add_participant():
-    data = request.json
+    data = request.get_json()
     result = supabase.table("participants").insert(data).execute()
-    return jsonify(result.data)
+    return 201, "Created", jsonify({"participant-id": data["id"]})
 
 
-@app.put("/participants")
-def update_participant():
-    data = request.json
-    wristband_id = data.get("wristband_id")
-    pid = data.get("id")
+@app.put("/participants/<pid>/wristband")
+def update_participant(pid):
+    data = request.get_json()
+    wristband_id = data["wid"]
     data = (
         supabase.table("participants")
         .update({"wristband_id": wristband_id})
         .eq("id", pid)
         .execute()
     )
-    return data.data
+    return (
+        200,
+        "OK",
+        jsonify(
+            {"message": f"Wristband successfully assigned to participant with ID {pid}"}
+        ),
+    )
 
 
-@app.post("/events/checkin/<event_id>")
+@app.post("/participants/events/checkin/<event_id>")
 def check_in(event_id):
-    data = request.json
-    pid = data.get("wristband_id")
-    log = (
+    input_data = request.get_json()
+    pid = input_data["participant_id"]
+    checkin_time = input_data["participant_id"]
+
+    data = (
         supabase.table("event_log")
-        .select("*")
-        .eq("user_id", pid)
-        .eq("event_id", event_id)
+        .insert({"user_id": pid, "event_id": event_id, "check_in_time": checkin_time})
         .execute()
     )
-    if len(log.data) > 0:
-        return jsonify(
-            {"message": "Already checked in", "code": "400", "hint": "", "details": ""}
-        )
-
-    supabase.table("event_log").insert({"user_id": pid, "event_id": event_id}).execute()
-
-    return data.data
-
-
-@app.get("/events/checkin/<event_id>")
-def get_event_logs(event_id):
-    logs = supabase.table("event_log").select("*").eq("event_id", event_id).execute()
-    return jsonify(logs.data)
+    log_id = data.data["id"]
+    return (
+        200,
+        "OK",
+        jsonify(
+            {
+                "log-id": log_id,
+                "message": f"Participant with ID {pid} checked in to event with log id {log_id} at {checkin_time}",
+            }
+        ),
+    )
