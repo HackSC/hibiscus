db0f8526a6a9273c5ed6bcdb95c5992e2c15941e
VictiniX888 <jiongyan@usc.edu>
2023-08-02 20:12:14 -0700

    Disable admin enpoints temporarily
    Until auth service is developed

--- apps/event-service/event_service/app.py
+++ apps/event-service/event_service/app.py
@@ -105,124 +105,124 @@ def remove_pinned_event(user_id: str):
 
 
 # Admin endpoints
-@app.post("/events")
-def add_event():
-    body = request.json
+# @app.post("/events")
+# def add_event():
+#     body = request.json
 
-    try:
-        # Convert dict to object
-        contact_info = body.pop("contactInfo", None)
-        event = data_types.EventAdmin(
-            eventId=None,
-            **body,
-            contactInfo=[data_types.Contact(**x) for x in contact_info],
-        )
+#     try:
+#         # Convert dict to object
+#         contact_info = body.pop("contactInfo", None)
+#         event = data_types.EventAdmin(
+#             eventId=None,
+#             **body,
+#             contactInfo=[data_types.Contact(**x) for x in contact_info],
+#         )
 
-        event_id = repository.add_event(event)
-        event.eventId = event_id
-        return jsonify(event), 200
-    except Exception as e:
-        return jsonify({"error": f"Failed to add event: {e}"}), 400
-
-
-@app.put("/events/<int:event_id>")
-def update_event(event_id: int):
-    body = request.json
-
-    try:
-        new_event = repository.update_event(
-            event_id,
-            name=body.get("eventName"),
-            description=body.get("description"),
-            start_time=body.get("startTime"),
-            end_time=body.get("endTime"),
-            location=body.get("location"),
-            bp_points=body.get("bpPoints"),
-            capacity=body.get("capacity"),
-            organizer_details=body.get("organizerDetails"),
-            event_tags=body.get("eventTags"),
-            industry_tags=body.get("industryTags"),
-        )
-        return jsonify(new_event), 200
-    except Exception as e:
-        return jsonify({"error": f"Failed to update event: {e}"}), 400
+#         event_id = repository.add_event(event)
+#         event.eventId = event_id
+#         return jsonify(event), 200
+#     except Exception as e:
+#         return jsonify({"error": f"Failed to add event: {e}"}), 400
 
 
-@app.post("/events/<int:event_id>/event-tags")
-def add_event_tag(event_id: int):
-    body = request.json
+# @app.put("/events/<int:event_id>")
+# def update_event(event_id: int):
+#     body = request.json
 
-    try:
-        repository.add_event_tag(event_id, body.get("eventTag"))
-        return jsonify({"eventTag": body.get("eventTag")}), 200
-    except Exception as e:
-        return jsonify({"error": f"Failed to add tag: {e}"}), 400
-
-
-@app.delete("/events/<int:event_id>/event-tags")
-def remove_event_tag(event_id: int):
-    body = request.json
-
-    try:
-        repository.remove_event_tag(event_id, body.get("eventTag"))
-        return jsonify({"eventTag": body.get("eventTag")}), 200
-    except Exception as e:
-        return jsonify({"error": f"Failed to remove tag: {e}"}), 400
-
-
-@app.post("/events/<int:event_id>/industry-tags")
-def add_industry_tag(event_id: int):
-    body = request.json
-
-    try:
-        repository.add_industry_tag(event_id, body.get("industryTag"))
-        return jsonify({"industryTag": body.get("industryTag")}), 200
-    except Exception as e:
-        return jsonify({"error": f"Failed to add tag: {e}"}), 400
+#     try:
+#         new_event = repository.update_event(
+#             event_id,
+#             name=body.get("eventName"),
+#             description=body.get("description"),
+#             start_time=body.get("startTime"),
+#             end_time=body.get("endTime"),
+#             location=body.get("location"),
+#             bp_points=body.get("bpPoints"),
+#             capacity=body.get("capacity"),
+#             organizer_details=body.get("organizerDetails"),
+#             event_tags=body.get("eventTags"),
+#             industry_tags=body.get("industryTags"),
+#         )
+#         return jsonify(new_event), 200
+#     except Exception as e:
+#         return jsonify({"error": f"Failed to update event: {e}"}), 400
 
 
-@app.delete("/events/<int:event_id>/industry-tags")
-def remove_industry_tag(event_id: int):
-    body = request.json
+# @app.post("/events/<int:event_id>/event-tags")
+# def add_event_tag(event_id: int):
+#     body = request.json
 
-    try:
-        repository.remove_industry_tag(event_id, body.get("industryTag"))
-        return jsonify({"industryTag": body.get("industryTag")}), 200
-    except Exception as e:
-        return jsonify({"error": f"Failed to remove tag: {e}"}), 400
-
-
-@app.post("/events/<int:event_id>/contacts")
-def add_contact(event_id: int):
-    body = request.json
-
-    try:
-        contact_id = repository.add_contact(event_id, **body)
-        return jsonify({"contactId": contact_id}), 200
-    except Exception as e:
-        return jsonify({"error": f"Failed to add contact: {e}"}), 400
+#     try:
+#         repository.add_event_tag(event_id, body.get("eventTag"))
+#         return jsonify({"eventTag": body.get("eventTag")}), 200
+#     except Exception as e:
+#         return jsonify({"error": f"Failed to add tag: {e}"}), 400
 
 
-@app.delete("/events/<int:event_id>/contacts")
-def remove_contact(event_id: int):
-    body = request.json
+# @app.delete("/events/<int:event_id>/event-tags")
+# def remove_event_tag(event_id: int):
+#     body = request.json
 
-    try:
-        repository.remove_contact(body.get("contactId"))
-        return jsonify({"contactId": body.get("contactId")}), 200
-    except Exception as e:
-        return jsonify({"error": f"Failed to remove contact: {e}"}), 400
+#     try:
+#         repository.remove_event_tag(event_id, body.get("eventTag"))
+#         return jsonify({"eventTag": body.get("eventTag")}), 200
+#     except Exception as e:
+#         return jsonify({"error": f"Failed to remove tag: {e}"}), 400
 
 
-@app.get("/events/<int:event_id>/pinned")
-def get_rsvps(event_id: int):
-    try:
-        rsvps = repository.get_rsvp_users(
-            event_id,
-            page=request.args.get("page"),
-            page_size=request.args.get("pageSize"),
-        )
+# @app.post("/events/<int:event_id>/industry-tags")
+# def add_industry_tag(event_id: int):
+#     body = request.json
 
-        return jsonify({"page": request.args.get("page"), "participants": rsvps}), 200
-    except Exception as e:
-        return jsonify({"error": f"Failed to get RSVPs: {e}"}), 400
+#     try:
+#         repository.add_industry_tag(event_id, body.get("industryTag"))
+#         return jsonify({"industryTag": body.get("industryTag")}), 200
+#     except Exception as e:
+#         return jsonify({"error": f"Failed to add tag: {e}"}), 400
+
+
+# @app.delete("/events/<int:event_id>/industry-tags")
+# def remove_industry_tag(event_id: int):
+#     body = request.json
+
+#     try:
+#         repository.remove_industry_tag(event_id, body.get("industryTag"))
+#         return jsonify({"industryTag": body.get("industryTag")}), 200
+#     except Exception as e:
+#         return jsonify({"error": f"Failed to remove tag: {e}"}), 400
+
+
+# @app.post("/events/<int:event_id>/contacts")
+# def add_contact(event_id: int):
+#     body = request.json
+
+#     try:
+#         contact_id = repository.add_contact(event_id, **body)
+#         return jsonify({"contactId": contact_id}), 200
+#     except Exception as e:
+#         return jsonify({"error": f"Failed to add contact: {e}"}), 400
+
+
+# @app.delete("/events/<int:event_id>/contacts")
+# def remove_contact(event_id: int):
+#     body = request.json
+
+#     try:
+#         repository.remove_contact(body.get("contactId"))
+#         return jsonify({"contactId": body.get("contactId")}), 200
+#     except Exception as e:
+#         return jsonify({"error": f"Failed to remove contact: {e}"}), 400
+
+
+# @app.get("/events/<int:event_id>/pinned")
+# def get_rsvps(event_id: int):
+#     try:
+#         rsvps = repository.get_rsvp_users(
+#             event_id,
+#             page=request.args.get("page"),
+#             page_size=request.args.get("pageSize"),
+#         )
+
+#         return jsonify({"page": request.args.get("page"), "participants": rsvps}), 200
+#     except Exception as e:
+#         return jsonify({"error": f"Failed to get RSVPs: {e}"}), 400
