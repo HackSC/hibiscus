43adc638d5d4140e036a2f5d85e65a98d918b6c0
Nick Raskop <raskop@usc.edu>
2023-09-15 21:36:45 -0700

    supabase 1 api endpoint to retrieve user object

--- apps/supabase-auth/src/index.ts
+++ apps/supabase-auth/src/index.ts
@@ -1,7 +1,28 @@
-import { Hono } from 'hono'
+import { Hono } from 'hono';
+import { createClient } from '@supabase/supabase-js';
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => c.text('Hello Hono!'))
+app.get('/me/:access_token', async (c) => {
+  const supabasePublicKey = 'placeholder';
+  const supabaseUrl = 'placeholder';
+  const options = {
+    auth: {
+      autoRefreshToken: true,
+      persistSession: false, // True by default, express needs set to false to work
+    },
+  };
+  const supabase = await createClient(supabaseUrl, supabasePublicKey, options);
 
-export default app
+  const access_token = c.req.param('access_token');
+  const getUserResponse = await supabase.auth.getUser(access_token);
+  const user_id = getUserResponse.data.user?.id;
+  const { data: userData } = await supabase
+    .from('user_profiles')
+    .select()
+    .eq('user_id', user_id);
+
+  return c.json(userData);
+});
+
+export default app;
