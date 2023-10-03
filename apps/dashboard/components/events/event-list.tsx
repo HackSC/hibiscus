26f71e8d1ffd37aa421cdfe755d76e2db1d7764e
Jiong Yan Yap <jiongyan@usc.edu>
2023-10-02 22:09:00 -0700

    Add styling for event service mobile list

--- apps/dashboard/components/events/event-list.tsx
+++ apps/dashboard/components/events/event-list.tsx
@@ -10,6 +10,10 @@ import '@sandstreamdev/react-swipeable-list/dist/styles.css';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import styles from './event-list.module.css';
 import styled from 'styled-components';
+import { Colors2023 } from '@hibiscus/styles';
+import { BoldText, Text } from '@hibiscus/ui';
+import { ImCross } from 'react-icons/im';
+import { Event } from '../../common/events.utils';
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 interface EventListProps {}
@@ -26,20 +30,20 @@ function EventList(props: EventListProps) {
     { id: uuidv4(), text: 'Item 4' },
   ]);
 
+  const [expandedEvent, setExpandedEvent] = useState<number | null>(null);
+
   const deleteItemById = (id: string) =>
     setItems((items) => items.filter((item) => item.id !== id));
 
   const addItem = () =>
     setItems([...items, { id: uuidv4(), text: `New item` }]);
 
-  const swipeRightOptions = (id: string): ISwipeActionProps => ({
-    content: <BasicSwipeContent $position="left">Delete</BasicSwipeContent>,
-    actionAnimation: contentAnimation,
-    action: () => deleteItemById(id),
-  });
-
   const swipeLeftOptions = (id: string): ISwipeActionProps => ({
-    content: <BasicSwipeContent $position="right">Delete</BasicSwipeContent>,
+    content: (
+      <BasicSwipeContent $position="right">
+        <ImCross />
+      </BasicSwipeContent>
+    ),
     actionAnimation: contentAnimation,
     action: () => deleteItemById(id),
   });
@@ -49,7 +53,7 @@ function EventList(props: EventListProps) {
 
   return (
     <>
-      <div className={styles['animations-swipeable-list__container']}>
+      <SwipeableListContainer>
         <SwipeableList threshold={threshold}>
           {({
             className,
@@ -61,10 +65,16 @@ function EventList(props: EventListProps) {
               className={className}
               enter={listAnimations}
               exit={listAnimations}
+              style={{ display: 'flex', flexDirection: 'column', gap: '1rem' }}
             >
               {items.map(({ id, text }) => (
                 <CSSTransition
-                  classNames={styles['my-node']}
+                  classNames={{
+                    enter: styles['my-node-enter'],
+                    enterActive: styles['my-node-enter-active'],
+                    exit: styles['my-node-exit'],
+                    exitActive: styles['my-node-exit-active'],
+                  }}
                   key={id}
                   timeout={transitionTimeout}
                 >
@@ -72,18 +82,29 @@ function EventList(props: EventListProps) {
                     key={id}
                     scrollStartThreshold={scrollStartThreshold}
                     swipeLeft={swipeLeftOptions(id)}
-                    swipeRight={swipeRightOptions(id)}
                     swipeStartThreshold={swipeStartThreshold}
                     threshold={threshold}
                   >
-                    <BasicListItem>{text}</BasicListItem>
+                    <EventCard
+                      event={{
+                        eventId: 12345,
+                        eventName: 'Puppy Pen',
+                        startTime: new Date(),
+                        endTime: new Date(),
+                        location: 'THH 102',
+                        bpPoints: 100,
+                      }}
+                      isExpanded={expandedEvent === 12345}
+                      onClick={() => setExpandedEvent(12345)}
+                      openModal={() => {}}
+                    />
                   </SwipeableListItem>
                 </CSSTransition>
               ))}
             </TransitionGroup>
           )}
         </SwipeableList>
-      </div>
+      </SwipeableListContainer>
       <button className="page__button" onClick={addItem}>
         Add item
       </button>
@@ -93,27 +114,126 @@ function EventList(props: EventListProps) {
 
 export default EventList;
 
-const BasicListItem = styled.div`
-  border-bottom: 1px solid #c4c4c4;
-  height: 32px;
+const SwipeableListContainer = styled.div`
   width: 100%;
-  display: flex;
-  align-items: center;
-  padding: 4px 8px;
-  user-select: none;
-  cursor: pointer;
+  margin: 24px 0;
+  color: #336675;
 `;
 
 const BasicSwipeContent = styled.div<{ $position: 'left' | 'right' }>`
-  background-color: red;
+  background-color: ${Colors2023.RED.DARK};
   box-sizing: border-box;
   color: white;
   height: 100%;
-  width: 100%;
   display: flex;
   align-items: center;
-  padding: 4px 8px;
+  padding: 1.5rem;
+
+  border-radius: 15px;
 
   justify-content: ${(props) =>
     props.$position === 'left' ? 'flex-start' : 'flex-end'};
+`;
+
+interface EventCardProps {
+  event: Event;
+  isExpanded: boolean;
+  onClick: () => void;
+  openModal: (eventId: number) => void;
+}
+
+function EventCard(props: EventCardProps) {
+  const startTime = props.event.startTime.toLocaleTimeString([], {
+    hour: '2-digit',
+    minute: '2-digit',
+  });
+  const endTime = props.event.endTime.toLocaleTimeString([], {
+    hour: '2-digit',
+    minute: '2-digit',
+  });
+
+  if (!props.isExpanded) {
+    return (
+      <CardClosed onClick={props.onClick}>
+        <BoldText>Puppy Pen</BoldText>
+        <Text>11:00 AM – 1:00 PM</Text>
+      </CardClosed>
+    );
+  }
+
+  return (
+    <CardOpen onClick={() => props.openModal(props.event.eventId)}>
+      <CardLeft />
+
+      <CardRight>
+        <Row>
+          <BoldText style={{ color: '#336675' }}>
+            {props.event.eventName}
+          </BoldText>
+          <BoldText style={{ color: 'black', fontSize: '0.75rem' }}>
+            {props.event.bpPoints} PTS
+          </BoldText>
+        </Row>
+        <Text style={{ color: Colors2023.GRAY.MEDIUM, fontSize: '0.75rem' }}>
+          {startTime} - {endTime}
+        </Text>
+        <Text style={{ color: Colors2023.GRAY.MEDIUM, fontSize: '0.75rem' }}>
+          {props.event.location}
+        </Text>
+      </CardRight>
+    </CardOpen>
+  );
+}
+
+const CardClosed = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  user-select: none;
+  cursor: pointer;
+  background-color: ${Colors2023.GRAY.LIGHT};
+
+  border-radius: 15px;
+
+  padding: 1rem 2rem;
+`;
+
+const CardOpen = styled.div`
+  display: flex;
+
+  align-items: stretch;
+
+  border: 2px solid ${Colors2023.GRAY.SCHEMDIUM};
+  border-radius: 15px;
+
+  overflow: hidden;
+
+  cursor: pointer;
+
+  width: 100%;
+`;
+
+const CardLeft = styled.div`
+  background-color: #336675;
+
+  width: 10px;
+`;
+
+const CardRight = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  background-color: ${Colors2023.GRAY.LIGHT};
+
+  padding: 1rem;
+  height: 100%;
+  width: 100%;
+`;
+
+const Row = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
 `;
