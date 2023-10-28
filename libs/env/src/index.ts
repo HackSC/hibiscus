abd095ab3b12c0e0918d603bff41a6bbcc39465f
Jiong Yan Yap <jiongyan@usc.edu>
2023-10-28 14:49:22 -0700

    Initialize judge fake user logic

--- libs/env/src/index.ts
+++ libs/env/src/index.ts
@@ -1,3 +1,16 @@
+import { HibiscusRole } from '@hibiscus/types';
+
+const DefaultRole = (() => {
+  if (process.env.SSO_DEFAULT_ROLE != null) {
+    const role = parseInt(process.env.SSO_DEFAULT_ROLE);
+    if (!isNaN(role)) {
+      return role;
+    }
+    // Default role = HACKER
+    return Object.keys(HibiscusRole).indexOf(HibiscusRole.HACKER) + 1;
+  }
+})();
+
 export const getEnv = () => {
   return {
     Hibiscus: {
@@ -27,6 +40,7 @@ export const getEnv = () => {
         ssoMockApp: process.env.NEXT_PUBLIC_SSO_MOCK_APP_URL,
         portal: process.env.NEXT_PUBLIC_PORTAL_URL,
         defaultRedirect: process.env.NEXT_PUBLIC_SSO_DEFAULT_REDIRECT_URL,
+        Podium: process.env.NEXT_PUBLIC_HIBISCUS_PODIUM_API_URL,
       },
       Hackform: {
         HackerAppResponsesTable:
@@ -43,6 +57,10 @@ export const getEnv = () => {
       Discord: {
         ApiUrl: process.env.NEXT_PUBLIC_DISCORD_API_URL,
         InviteUrl: process.env.NEXT_PUBLIC_DISCORD_INVITE_URL,
+      },
+      SSO: {
+        DefaultRole,
+        DefaultJudgeVertical: process.env.JUDGE_DEFAULT_VERTICAL,
       },
     },
   };
--- libs/sso-client/src/lib/sso-client.ts
+++ libs/sso-client/src/lib/sso-client.ts
@@ -318,6 +318,8 @@ async function initializeFakeUser(access_token: string, refresh_token: string) {
     const email = faker.internet.email();
     const password = faker.internet.password();
 
+    const role = getEnv().Hibiscus.SSO.DefaultRole;
+
     if (user == null || session == null) {
       ({
         data: { user },
@@ -328,6 +330,14 @@ async function initializeFakeUser(access_token: string, refresh_token: string) {
       }));
 
       if (user !== null) {
+        if (role === 7) {
+          // Judge
+          await initializeJudgeUser(
+            user.id,
+            getEnv().Hibiscus.SSO.DefaultJudgeVertical
+          );
+        }
+
         ({
           data: { user, session },
         } = await supabase.auth.signInWithPassword({
@@ -343,8 +353,7 @@ async function initializeFakeUser(access_token: string, refresh_token: string) {
         email: user.email,
         first_name: faker.name.firstName(),
         last_name: faker.name.lastName(),
-        // Default role = HACKER
-        role: Object.keys(HibiscusRole).indexOf(HibiscusRole.HACKER) + 1,
+        role,
       });
 
       return [session.access_token, session.refresh_token];
@@ -352,6 +361,24 @@ async function initializeFakeUser(access_token: string, refresh_token: string) {
   }
 
   return [access_token, refresh_token];
+}
+
+/**
+ * Additional initialization steps for new judge fake user
+ */
+async function initializeJudgeUser(userId: string, verticalId?: string) {
+  const env = getEnv();
+
+  // Must use fetch in middleware
+  const res = await fetch(`${env.Hibiscus.AppURL.Podium}/judges/${userId}`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ verticalId }),
+  });
+
+  if (!res.ok) {
+    console.log(res);
+  }
 }
 
 function createSupabaseServiceClient(): SupabaseClient {
