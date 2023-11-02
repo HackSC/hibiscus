7e63d7498ec4cf292befedd8221ed0ab1395675c
Jiong Yan Yap <jiongyan@usc.edu>
2023-11-01 23:11:54 -0700

    Add sponsor access to tables

---
+++ supabase/migrations/20231101000000_add_sponsor_access_events.sql
@@ -0,0 +1,44 @@
+create or replace function get_sponsors()
+returns setof uuid
+language sql
+security definer
+set search_path = public
+stable
+as $$
+    select user_id 
+    from user_profiles
+    where user_profiles.role = 4
+$$;
+
+create policy "Allow sponsors to access all rows"
+    on public.user_profiles
+    for select
+    to authenticated
+    using (auth.uid() in (
+        select get_sponsors()
+    ));
+
+create policy "Allow sponsors to access all rows"
+  on public.event_log
+  for select 
+  to authenticated
+  using (auth.uid() in (
+      select get_sponsors()
+  ))
+
+create policy "Allow sponsors to access all rows"
+    on public.events
+    for select 
+    to authenticated
+    using (auth.uid() in (
+        select get_sponsors()
+    ));
+
+create policy "Allow sponsors to access all rows"
+    on public.participants
+    for select 
+    to authenticated
+    using (auth.uid() in (
+        select get_sponsors()
+    ))
+    
