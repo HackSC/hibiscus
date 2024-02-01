52c679669f3a1aff37b904c9d98f08c83e9a1585
Thomas Cazort <thomas.cazort@gmail.com>
2024-02-01 12:15:23 -0800

    DB call function stubs

---
+++ apps/event-service-next/src/utils/repository.ts
@@ -0,0 +1,101 @@
+function getEvent(event_id: string) {
+  return -1;
+}
+
+function getEventAdmin(event_id: string) {
+  return -1;
+}
+
+function getEvents(
+  page: number = null,
+  page_size: number = null,
+  date?: Date,
+  after?: Date,
+  name?: string,
+  location?: string
+) {
+  return -1;
+}
+
+function addEvent(event: any) {
+  return -1;
+}
+
+function updateEvent(
+  event_id: string,
+  event_tags?: string[],
+  industry_tags?: string[]
+) {
+  return -1;
+}
+
+function deleteEvent(event_id: string) {
+  return -1;
+}
+
+function getPinnedEvents(user_id: string) {
+  return -1;
+}
+
+function addPinnedEvent(user_id: string, event_id: string) {
+  return -1;
+}
+
+function removePinnedEvent(user_id: string, event_id: string) {
+  return -1;
+}
+
+function getRSVPUsers(event_id: string) {
+  return -1;
+}
+
+function addEventTag(event_id: string, event_tag: string) {
+  return -1;
+}
+
+function removeEventTag(event_id: string, event_tag: string) {
+  return -1;
+}
+
+function addIndustryTag(event_id: string, industry_tag: string) {
+  return -1;
+}
+
+function removeIndustryTag(event_id: string, industry_tag: string) {
+  return -1;
+}
+
+function addContact(
+  event_id: string,
+  name: string,
+  role?: string,
+  phone?: string,
+  email?: string
+) {
+  return -1;
+}
+
+function removeContact(contact_id: string) {
+  return -1;
+}
+
+const repository = {
+  getEvent,
+  getEventAdmin,
+  getEvents,
+  addEvent,
+  updateEvent,
+  deleteEvent,
+  getPinnedEvents,
+  addPinnedEvent,
+  removePinnedEvent,
+  getRSVPUsers,
+  addEventTag,
+  removeEventTag,
+  addIndustryTag,
+  removeIndustryTag,
+  addContact,
+  removeContact,
+};
+
+export default repository;
--- libs/event-service-next/README.md
+++
@@ -1,7 +0,0 @@
-# event-service-next
-
-This library was generated with [Nx](https://nx.dev).
-
-## Running unit tests
-
-Run `nx test event-service-next` to execute the unit tests via [Jest](https://jestjs.io).
