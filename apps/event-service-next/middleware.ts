3c0598296c00a5c2d6739763fb1af36e0e2fa910
Thomas Cazort <thomas.cazort@gmail.com>
2024-03-05 22:48:00 -0800

    API middleware testing

--- apps/event-service-next/middleware.ts
+++ apps/event-service-next/middleware.ts
@@ -5,10 +5,12 @@ import type { NextRequest } from 'next/server';
 export async function middleware(request: NextRequest) {
   const path = request.nextUrl;
 
-  if (path.pathname.startsWith('/api/error')) {
+  if (
+    path.pathname.startsWith('/api/error') ||
+    path.pathname.startsWith('/api/health')
+  ) {
     return NextResponse.next();
   }
-  console.log('middleware');
   const authServiceUrl = getEnv().Hibiscus.EventService.authServiceUrl;
   const masterToken = getEnv().Hibiscus.EventService.masterToken;
 
@@ -16,6 +18,8 @@ export async function middleware(request: NextRequest) {
   let accessToken = headers.get('Authorization');
 
   if (!accessToken) {
+    // Works
+    console.log('no access token');
     request.nextUrl.searchParams.set('status', '403');
     request.nextUrl.searchParams.set('message', 'No access token provided');
     request.nextUrl.pathname = '/api/error';
@@ -23,6 +27,8 @@ export async function middleware(request: NextRequest) {
   }
 
   if (!accessToken.startsWith('Bearer ')) {
+    // Works
+    console.log('invalid token format');
     request.nextUrl.searchParams.set('status', '401');
     request.nextUrl.searchParams.set(
       'message',
@@ -35,13 +41,15 @@ export async function middleware(request: NextRequest) {
   accessToken = accessToken.split('Bearer ')[1];
 
   if (accessToken === masterToken) {
+    // Works
+    console.log('master token');
     return NextResponse.next();
   }
-
   const apiUrl = `${authServiceUrl}/verify-token/${accessToken}`;
   const response = await fetch(apiUrl);
 
   if (response.status !== 200) {
+    console.log('invalid token');
     request.nextUrl.searchParams.set('status', '401');
     request.nextUrl.searchParams.set('message', 'Invalid access token');
     request.nextUrl.pathname = '/api/error';
@@ -50,21 +58,34 @@ export async function middleware(request: NextRequest) {
 
   try {
     const data = await response.json();
+    console.log('data', data);
     const role = data.role;
     const method = request.method;
     if (role === 'SUPERADMIN') {
+      console.log('superadmin');
       return NextResponse.next();
     }
+    console.log('path', path.pathname);
     if (
-      (path.pathname.startsWith('/api/events') && method === 'GET') ||
-      (path.pathname.startsWith('/api/events/') && method === 'GET')
+      // Test /api/event-id
+      (path.pathname === '/api/events' && method === 'GET') ||
+      (path.pathname.startsWith('/api/events/') &&
+        path.pathname.split('/').length === 3 &&
+        method === 'GET')
     ) {
+      console.log('bypass');
       return NextResponse.next(); // Bypass the middleware for these routes
-    } else if (path.pathname.includes('{user-id}')) {
-      const userId = path.pathname.split('/')[2];
-      if (userId === data.userId) {
+    } else if (path.pathname.startsWith('/api/pinned-events')) {
+      console.log('HEREREREREE');
+      const userId = path.pathname.split('/')[3];
+      console.log('userId', userId);
+      console.log('data.userId', data.userId);
+      if (userId === data.user_id) {
+        // Works
+        console.log('match');
         return NextResponse.next();
       } else {
+        // Works
         request.nextUrl.searchParams.set('status', '403');
         request.nextUrl.searchParams.set(
           'message',
