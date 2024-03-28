565a794c7d5025125134389b96e5e975a0342ecd
roryvision <104661206+roryvision@users.noreply.github.com>
2024-03-28 10:42:47 -0700

    Implement middleware

--- apps/podium-service/middleware.ts
+++ apps/podium-service/middleware.ts
@@ -2,8 +2,8 @@ import { getEnv } from '@hibiscus/env';
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
-export async function middleware(request: NextRequest) {
-  const path = request.nextUrl;
+export async function middleware(req: NextRequest) {
+  const path = req.nextUrl;
 
   if (
     path.pathname.startsWith('/api/error') ||
@@ -15,24 +15,24 @@ export async function middleware(request: NextRequest) {
   const authServiceUrl = getEnv().Hibiscus.Podium.ApiUrl;
   const masterToken = getEnv().Hibiscus.Podium.ApiMasterToken;
 
-  const headers = request.headers;
+  const headers = req.headers;
   let accessToken = headers.get('Authorization');
 
   if (!accessToken) {
-    request.nextUrl.searchParams.set('status', '403');
-    request.nextUrl.searchParams.set('message', 'No access token provided');
-    request.nextUrl.pathname = '/api/error';
-    return NextResponse.redirect(request.nextUrl);
+    req.nextUrl.searchParams.set('status', '403');
+    req.nextUrl.searchParams.set('message', 'No access token provided');
+    req.nextUrl.pathname = '/api/error';
+    return NextResponse.redirect(req.nextUrl);
   }
 
   if (!accessToken.startsWith('Bearer')) {
-    request.nextUrl.searchParams.set('status', '401');
-    request.nextUrl.searchParams.set(
+    req.nextUrl.searchParams.set('status', '401');
+    req.nextUrl.searchParams.set(
       'message',
       'Invalid authentication token format.'
     );
-    request.nextUrl.pathname = '/api/error';
-    return NextResponse.redirect(request.nextUrl);
+    req.nextUrl.pathname = '/api/error';
+    return NextResponse.redirect(req.nextUrl);
   }
 
   accessToken = accessToken.split('Bearer ')[1];
@@ -40,44 +40,60 @@ export async function middleware(request: NextRequest) {
   if (accessToken === masterToken) {
     return NextResponse.next();
   }
-  const response = await fetch(`${authServiceUrl}/verify-token/${accessToken}`);
 
-  if (response.status !== 200) {
-    request.nextUrl.searchParams.set('status', '401');
-    request.nextUrl.searchParams.set('message', 'Invalid access token');
-    request.nextUrl.pathname = '/api/error';
-    return NextResponse.redirect(request.nextUrl);
+  const res = await fetch(`${authServiceUrl}/verify-token/${accessToken}`);
+
+  if (res.status !== 200) {
+    req.nextUrl.searchParams.set('status', '401');
+    req.nextUrl.searchParams.set('message', 'Invalid access token');
+    req.nextUrl.pathname = '/api/error';
+    return NextResponse.redirect(req.nextUrl);
   }
 
-  const judgeRoutes = ['/verticals', '/projects', '/ranking', '/notes', '/comments', '/judges'];
-  const isAccessingJudgeRoute = judgeRoutes.some((route) => path.pathname.startsWith('/api' + route));
+  const judgeRoutes = [
+    { route: '/verticals', methods: ['GET'] },
+    { route: '/projects', methods: ['GET'] },
+    {
+      route: '/ranking/[verticalId]/[userId]',
+      methods: ['POST', 'GET', 'DELETE'],
+    },
+    {
+      route: '/notes/[verticalId]/[projectId]/[userId]',
+      methods: ['POST', 'GET'],
+    },
+    { route: '/comments/[projectId]/user/[userId]', methods: ['POST'] },
+    { route: '/comments/id/[commentId]', methods: ['PUT'] },
+    { route: '/comments/[projectId]', methods: ['GET'] },
+    { route: '/judges/[judgeId]', methods: ['GET'] },
+  ];
+
+  const isAccessingJudgeRoute = judgeRoutes.some(({ route, methods }) => {
+    const regex = new RegExp(`^/api${route.replace(/\[.*?\]/g, '[^/]+')}$`);
+    return regex.test(path.pathname) && methods.includes(req.method);
+  });
 
   try {
-    const data = await response.json();
+    const data = await res.json();
     const role = data.role;
-    const method = request.method;
 
     if (role === 'SUPERADMIN') {
       return NextResponse.next();
-    } else if (isAccessingJudgeRoute && role === 'JUDGE') { // TODO: Specify endpoints
+    } else if (isAccessingJudgeRoute && role === 'JUDGE') {
       return NextResponse.next();
     } else {
-      request.nextUrl.searchParams.set('status', '403');
-      request.nextUrl.searchParams.set(
+      req.nextUrl.searchParams.set('status', '403');
+      req.nextUrl.searchParams.set(
         'message',
         'Access denied. User does not have permissions.'
       );
-      request.nextUrl.pathname = '/api/error';
-      return NextResponse.redirect(request.nextUrl);
+      req.nextUrl.pathname = '/api/error';
+      return NextResponse.redirect(req.nextUrl);
     }
   } catch (error) {
-    request.nextUrl.searchParams.set('status', '500');
-    request.nextUrl.searchParams.set(
-      'message',
-      `Internal server error: ${error}`
-    );
-    request.nextUrl.pathname = '/api/error';
-    return NextResponse.redirect(request.nextUrl);
+    req.nextUrl.searchParams.set('status', '500');
+    req.nextUrl.searchParams.set('message', `Internal server error: ${error}`);
+    req.nextUrl.pathname = '/api/error';
+    return NextResponse.redirect(req.nextUrl);
   }
 }
 
