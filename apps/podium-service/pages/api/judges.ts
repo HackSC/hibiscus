968767ac8f19a34d0475b8220c580063225a25b7
roryvision <104661206+roryvision@users.noreply.github.com>
2024-03-28 11:35:25 -0700

    Un-implemented judge provisioning endpoints

--- apps/podium-service/pages/api/judges.ts
+++ apps/podium-service/pages/api/judges.ts
@@ -51,44 +51,8 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       } catch (error) {
         return res.status(500).json({ error: 'Internal Server Error' });
       }
-    // TODO: This is supposed to send an email not add it to the database? not sure
     case 'POST':
-      try {
-        const { email, name, password } = body;
-        if (!email || typeof email !== 'string') {
-          return res.status(400).json({ error: 'Invalid request! Email is required and must be a string.'});
-        }
-
-        let firstName, lastName;
-        if (typeof name !== undefined) {
-          [firstName, lastName] = name.split(' ');
-        } else {
-          return res.status(400).json({ error: 'Invalid request! Name is required and must be a string.'});
-        }
-
-        // TODO: Get user_id from user_profiles
-        const addJudge = {
-          user_id: null,
-          email: email,
-          first_name: firstName,
-          last_name: lastName,
-          role: 7,
-        };
-
-        // TODO: This should insert to judges, not user_profiles, we can assume the user exists already?
-        const { error } = await supabase
-          .schema('public')
-          .from('user_profiles')
-          .insert(addJudge);
-
-        if (error) {
-          throw new Error('Failed to add new judge');
-        }
-
-        return res.status(201).end();
-      } catch (error) {
-        return res.status(500).json({ error: 'Internal Server Error' });
-      }
+      return res.status(501).json({ error: 'Not implemented' });
     default:
       res.status(405).end();
       break;
--- apps/podium-service/pages/api/judges/bulk.ts
+++ apps/podium-service/pages/api/judges/bulk.ts
@@ -1,1 +1,13 @@
-// POST provision many new judges
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const { method } = req;
+
+  switch (method) {
+    case 'POST':
+      return res.status(501).json({ error: 'Not implemented' });
+    default:
+      res.status(405).end();
+      break;
+  }
+}
