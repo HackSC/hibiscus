4e1b7c43f3f6e7b2932a098a9610fc879701ea62
roryvision <104661206+roryvision@users.noreply.github.com>
2024-03-27 23:37:11 -0700

    Update vertical endpoint

--- apps/podium-service/pages/api/verticals/[verticalId].ts
+++ apps/podium-service/pages/api/verticals/[verticalId].ts
@@ -1,1 +1,43 @@
-// PUT update vertical details
+import { supabase } from 'apps/podium-service/libs/supabase';
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const { method, query, body } = req;
+  const verticalId = query.verticalId as string;
+  const projectId = query.projectId as string;
+
+  switch (method) {
+    case 'PUT':
+      const fields = ['name', 'description'];
+      const updateVertical = {};
+
+      try {
+        const {
+          name,
+          description,
+        } = body;
+
+        fields.forEach((field) => {
+          if (typeof body[field] !== undefined) {
+            updateVertical[field] = body[field];
+          }
+        })
+
+        const { error } = await supabase
+          .from('verticals')
+          .update(updateVertical)
+          .eq('vertical_id', verticalId);
+
+        if (error) {
+          throw error;
+        }
+
+        return res.status(200).end();
+      } catch (error) {
+        return res.status(500).json({ error: 'Internal Server Error' });
+      }
+    default:
+      res.status(405).end();
+      break;
+  }
+}
