bfdf3dad2e4716b594226cd2451fccaad4314638
roryvision <104661206+roryvision@users.noreply.github.com>
2024-03-27 23:39:26 -0700

    Comments endpoints

--- apps/podium-service/pages/api/comments/[projectId]/user/[userId].ts
+++ apps/podium-service/pages/api/comments/[projectId]/user/[userId].ts
@@ -1,1 +1,37 @@
-// POST comment for a project
+import { supabase } from 'apps/podium-service/libs/supabase';
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const { method, query, body } = req;
+  const projectId = query.projectId as string;
+  const userId = query.userId as string;
+
+  switch (method) {
+    case 'POST':
+      try {
+        const { comment } = body;
+        if (!comment || typeof comment !== 'string') {
+          return res.status(400).json({ error: 'Invalid request! Comment is required and must be a string.'});
+        }
+
+        const { error } = await supabase
+          .from('comments')
+          .insert({
+            project_id: projectId,
+            user_id: userId,
+            comment: comment
+          });
+
+        if (error) {
+          throw new Error('Failed to add new comment');
+        }
+
+        return res.status(201).end();
+      } catch (error) {
+        return res.status(500).json({ error: 'Internal Server Error' });
+      }
+    default:
+      res.status(405).end();
+      break;
+  }
+}
--- apps/podium-service/pages/api/comments/id/[commentId].ts
+++ apps/podium-service/pages/api/comments/id/[commentId].ts
@@ -1,1 +1,33 @@
-// PUT update comment for a project
+import { supabase } from 'apps/podium-service/libs/supabase';
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const { method, query, body } = req;
+  const commentId = query.commentId as string;
+
+  switch (method) {
+    case 'PUT':
+      try {
+        const { comment } = body;
+        if (!comment || typeof comment !== 'string') {
+          return res.status(400).json({ error: 'Invalid request! Comment is required and must be a string.'});
+        }
+
+        const { error } = await supabase
+          .from('comments')
+          .update({ comment: comment })
+          .eq('comment_id', commentId);
+
+        if (error) {
+          throw new Error('Failed to edit comment');
+        }
+
+        return res.status(204).end();
+      } catch (error) {
+        return res.status(500).json({ error: 'Internal Server Error' });
+      }
+    default:
+      res.status(405).end();
+      break;
+  }
+}
